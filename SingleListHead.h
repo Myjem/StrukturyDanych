b1bd26712ce25a48c75cd7f69e2d76495b59fb83
MANDRW <mandrzejewski132@gmail.com>
2024-03-17 22:15:56 +0100

    update

---
+++ SingleListHead.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_CPP_SINGLELISTHEAD_H
+#define MAIN_CPP_SINGLELISTHEAD_H
+#include "node_single.h"
+class SingleListHead{
+private:
+    node_single*head;
+    int size;
+public:
+
+
+};
+#endif
