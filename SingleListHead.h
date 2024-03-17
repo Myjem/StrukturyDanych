6eb2c69b9620751502f18811bfdba228850a828e
MANDRW <mandrzejewski132@gmail.com>
2024-03-17 22:18:01 +0100

    update

--- SingleListHead.h
+++ SingleListHead.h
@@ -6,7 +6,16 @@ private:
     node_single*head;
     int size;
 public:
-
+    SingleListHead();
+    SingleListHead(int s);
+    void Append(int i,int data);
+    void PushBack(int data);
+    void DelBack(int data);
+    void PushFront(int data);
+    void DelFront(int data);
+    void Del(int i, int data);
+    int Search(int i);
+    ~SingleListHead();
 
 };
 #endif
