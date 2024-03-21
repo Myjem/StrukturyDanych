f64890288cd4bb772b4a296350e63ea742f2f7f8
Myjem <siepkawiktor240@gmail.com>
2024-03-21 19:28:05 +0100

    Implementacja DoubleList

--- DoubleList.h
+++ DoubleList.h
@@ -2,17 +2,18 @@
 
 class DoubleList{
 private:
-    node_double* head;
-    int size;
+    node_double* head = NULL;
+    node_double* tail = NULL;
+    int size = 0;
 public:
     DoubleList();
-    DoubleList(int s);
-    void append(int i,int data);
+    void append(int i,int data); //TODO
     void push_back(int data);
     void del_back();
     void push_front(int data);
-    void del_front();
-    void del(int i);
-    int search(int data);
+    void del_front(); 
+    void del(int i); //TODO
+    node_double* search(int data);
+    void printdata();
     ~DoubleList();
 };
