8d95a8d98dfa5d4fe8ac3ce8cdea0765a010327e
MANDRW <mandrzejewski132@gmail.com>
2024-03-24 22:54:54 +0100

    number generator

--- number_generator.cpp
+++ number_generator.cpp
@@ -1,10 +1,19 @@
 #include<iostream>
 #include<fstream>
+#include<cstdlib>
+#include<ctime>
+#include<cstdio>
 
 using namespace std;
 
 int main(){
-
-
+    fstream file;
+    srand(time(NULL));
+    file.open("test_data_small.txt",ios::out);
+    for(int i=0;i<100000;i++) file<<(rand()%21)-10<<endl;
+    file.close();
+    file.open("test_data_big.txt",ios::out);
+    for(int i=0;i<100000;i++) file<<(rand()%999000001)+1000000<<endl;
+    file.close();
     return 0;
 }
