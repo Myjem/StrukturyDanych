ac2672c5e23ba3363d21e8fb929c3a9e5d744767
Wiktor Siepka <siepkawiktor240@gmail.com>
2024-03-25 12:09:20 +0100

    cos tam napisalem ale mi sie nie chce, ide sluchac wykladu

--- UI.cpp
+++ UI.cpp
@@ -25,6 +25,75 @@ void Ui(){
                 cout<<"END";
                 return ;
             }
+            case 1:{
+                while(True)
+                {
+                    cout << "Wybierz operacje" << endl;
+                    cout << "[1] ";
+
+                }
+
+            }
+            case 2:{
+
+            }
+            case 3:{
+                while(True)
+                {
+                    int n;
+                    cout << "Wybierz operacje" << endl;
+                    cout << "[1] Stwórz pustą liste" << endl;
+                    cout << "[2] Stwórz listę z losowymi danymi" << endl;
+                    cout << "[3] Dodaj na początek listy" << endl;
+                    cout << "[4] Dodaj na koniec listy" << endl;
+                    cout << "[5] Dodaj w dowolym miejscu" << endl;
+                    cout << "[6] Usuń pierwszy element" << endl;                        //dobra nie chce mi sie tego robic na wykladzie, ide chwile posluchac co on gada
+                    cout << "[7] Usuń ostatni element" << endl;
+                    cout << "[8] Usuń dowolny element" << endl;
+                    cout << "[9] Przeszukaj liste" << endl;
+                    cout << "[10] Wróć" << endl;
+                    cout << "Wybór" << end;
+                    cin >> n;
+                    cout << endl;
+                    switch(n){
+                        case 1:{
+
+                        }
+                        case 2:{
+                            
+                        }
+                        case 3:{
+                            
+                        }
+                        case 4:{
+                            
+                        }
+                        case 5:{
+                            
+                        }
+                        case 6:{
+                            
+                        }
+                        case 7:{
+                            
+                        }
+                        case 8:{
+                            
+                        }
+                        case 9:{
+                            
+                        }
+                        case 10:{
+                            
+                        }
+                    }
+
+                }
+
+            }
+            case 4: {
+
+            }      
         }
 
         //menu wyswietlanie wyboru struktury
